1f3032ac3bc8de9bf9be799d17df4129d331a21d
kumangkumeng <kumangxxx@gits.co.id>
2016-06-16 09:34:20 +0700

    fix ;

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ app.get('/', function(req, res) {
 app.get('/optimise', function(req, res)) {
   res.status(1102);
   res.json({'foo' : 'bar'});
-}
+};
 
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
